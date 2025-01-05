8d6bac472fb85f11f75462fae9623a6b11b4fcd7
kitajusSus <kitaj2458@gmail.com>
2025-01-05 21:46:41 +0100

    mm function works

--- 000_example.zig
+++ 000_example.zig
@@ -1,31 +1,39 @@
+// japierdole kocham zig i ziggersów
 const std = @import("std");
 
-
-/// Multiplications of two marices
-
-fn mm( A: [*]const f32, B: [*]const f32,  I: usize, J: usize,  K: usize ) ![]f32 {
+/// Multiplication of two matrices
+fn mm(A: []const f32, B: []const f32, I: usize, J: usize, K: usize) ![]const f32 {
     const allocator = std.heap.page_allocator;
-    var C = try allocator.alloc(f32, I*J);
-    defer allocator.free(C);
+    // Allocates memory for the resulting matrix
+    var C = try allocator.alloc(f32, I * J);
+    // Matrix multiplication
     var i: usize = 0;
-    while (i<I) : (i += 1) {
+    while (i < I) : (i += 1) {
         var j: usize = 0;
-        while (j<J) : (j += 1){
+        while (j < J) : (j += 1) {
             var sum: f32 = 0;
             var k: usize = 0;
-            while (k<K) : (k+=1){
-                sum += A[i * K + k] *  B[k*J + j];
-            } 
-            C[i*J + j] = sum; 
+            while (k < K) : (k += 1) {
+                sum += A[i * K + k] * B[k * J + j];
+            }
+            C[i * J + j] = sum;
         }
     }
     return C;
 }
 
 pub fn main() !void {
-    const cos: [2*3]f32 = [_]f32{1, 2, 3, 4, 5, 6};
-    const inne: [3*2]f32 = [_]f32{1, 2, 3, 4, 5, 6};
-    const wynik = try mm(cos[0..], inne[0..],2,2,3) catch unreachable;
-    std.debug.print("C: {d}\n", .{wynik});
+    const cos: [2 * 3]f32 = [_]f32{1, 2, 3, 4, 5, 6}; // 2x3 matrix
+    const inne: [3 * 2]f32 = [_]f32{1, 2, 3, 4, 5, 6}; // 3x2 matrix
 
+    // using  mm() and TRY TO handle potential allocation error
+    const wynik = try mm(cos[0..], inne[0..], 2, 2, 3);
+
+    //RESULT
+    std.debug.print("Wynik: [{d}]\n", .{wynik});
+
+    // FreeING the memory 
+    const allocator = std.heap.page_allocator;
+    allocator.free(wynik);
 }
+
