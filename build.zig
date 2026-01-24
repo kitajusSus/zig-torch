7850d114a058d40c5d4f80370ee50fab3ea35813
kitajusSus <krzysztof.bezub@gmail.com>
2026-01-24 10:26:05 +0100

    build updates

--- build.zig
+++ build.zig
@@ -6,18 +6,20 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zigtorch",
-        .root_source_file = .{ .path = "src/mm.zig" },
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/mm.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     // Dodaj ZigTorch jako moduł
-    const zigtorch_dep = b.dependency("zigtorch", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const zigtorch_mod = zigtorch_dep.module("zigtorch");
-    exe.addModule("zigtorch", zigtorch_mod);
+    // const zigtorch_dep = b.dependency("zigtorch", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // const zigtorch_mod = zigtorch_dep.module("zigtorch");
+    // exe.addModule("zigtorch", zigtorch_mod);
 
     b.installArtifact(exe);
 }
